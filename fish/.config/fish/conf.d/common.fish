8791c3d94bed7637a19611ed1ee2c13bc58adee5
HP97 <phamh.2014@gmail.com>
2022-03-28 01:31:28 +1100

    Added new alias, new coc extensions and vim plugs

--- fish/.config/fish/conf.d/common.fish
+++ fish/.config/fish/conf.d/common.fish
@@ -2,8 +2,16 @@ fish_add_path ~/bin
 
 fish_add_path ~/.local/bin
 
+alias nvconf="nvim ~/.config/nvim/init.vim"
+alias nvcd="cd ~/.config/nvim"
 set -gx EDITOR nvim
 
 # Uses ~/.nvmrc to determine which node version to use for all sessions
 nvm use (cat ~/.nvmrc)
+
+if type -q wsl.exe
+    alias cpwd="pwd | clip.exe"
+else
+    alias cpwd="pwd | pbcopy"
+end
 
