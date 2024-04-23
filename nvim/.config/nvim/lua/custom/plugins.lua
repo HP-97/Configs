22c25095bbcc59fa8e17077726e9735d9da6b3f7
hoa <Hoa.Pham@ue.com.au>
2024-04-24 00:50:50 +1000

    update dependencies

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -66,8 +66,20 @@ local plugins = {
     config = function(_, opts)
       require("trouble").setup(opts)
     end
-  }
+  },
 
+  {
+    "ziontee113/icon-picker.nvim",
+    config = function()
+        require("icon-picker").setup({ disable_legacy_commands = true })
+
+        local opts = { noremap = true, silent = true }
+
+        vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+        vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
+        vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+    end
+}
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
